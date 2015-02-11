a7fe25c610f66ce3dabe1587c5796780d0c317c2
yuyingjiancn <yuyingjian@gmail.com>
2015-02-11 14:18:08 +0800

    0.0.1

---
+++ YuYingjian.SimpleCaptcha/Captcha.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace YuYingjian.SimpleCaptcha
+{
+    public class Captcha
+    {
+        //生成的验证码长度
+        public static int CodeLength { get; set; }
+
+        //生成的验证码宽度
+        public static int Width { get; set; }
+
+        //生成的验证码高度
+        public static int Height { get; set; }
+
+        //验证码字体大小
+        public static int FontSize { get; set; }
+
+        //生成的验证码
+        public string Code { get; private set; }
+
+        public byte[] ImageBytes { get; private set; }
+
+        public Captcha()
+        {
+            Code = CreateValidateCode(CodeLength);
+            ImageBytes = CreateValidateGraphic();
+        }
+
+        /// <summary>
+        /// 生成验证码
+        /// </summary>
+        /// <param name="length">指定验证码的长度</param>
+        /// <returns></returns>
+        private string CreateValidateCode(int length)
+        {
+            var randMembers = new int[length];
+            var validateNums = new int[length];
+            var validateNumberStr = "";
+            //生成起始序列值
+            var seekSeek = unchecked((int)DateTime.Now.Ticks);
+            var seekRand = new Random(seekSeek);
+            var beginSeek = (int)seekRand.Next(0, Int32.MaxValue - length * 10000);
+            var seeks = new int[length];
+            for (var i = 0; i < length; i++)
+            {
+                beginSeek += 10000;
+                seeks[i] = beginSeek;
+            }
+            //生成随机数字
+            for (var i = 0; i < length; i++)
+            {
+                var rand = new Random(seeks[i]);
+                var pownum = 1 * (int)Math.Pow(10, length);
+                randMembers[i] = rand.Next(pownum, Int32.MaxValue);
+            }
+            //抽取随机数字
+            for (var i = 0; i < length; i++)
+            {
+                var numStr = randMembers[i].ToString();
+                var numLength = numStr.Length;
+                var rand = new Random();
+                var numPosition = rand.Next(0, numLength - 1);
+                validateNums[i] = Int32.Parse(numStr.Substring(numPosition, 1));
+            }
+            //生成验证码
+            for (var i = 0; i < length; i++)
+            {
+                validateNumberStr += validateNums[i].ToString();
+            }
+            return validateNumberStr;
+        }
+
+        /// <summary>
+        /// 创建验证码的图片
+        /// </summary>
+        /// <param name="Code">验证码</param>
+        private byte[] CreateValidateGraphic()
+        {
+            var image = new Bitmap(Width, Height);
+            var g = Graphics.FromImage(image);
+            try
+            {
+                //生成随机生成器
+                var random = new Random();
+                //清空图片背景色
+                g.Clear(Color.White);
+                //画图片的干扰线
+                for (int i = 0; i < 25; i++)
+                {
+                    var x1 = random.Next(image.Width);
+                    var x2 = random.Next(image.Width);
+                    var y1 = random.Next(image.Height);
+                    var y2 = random.Next(image.Height);
+                    g.DrawLine(new Pen(Color.Silver), x1, y1, x2, y2);
+                }
+
+                var font = new Font("Arial", FontSize, (FontStyle.Bold | FontStyle.Italic));
+                var brush = new LinearGradientBrush(new Rectangle(0, 0, image.Width, image.Height),
+                Color.Blue, Color.DarkRed, 1.2f, true);
+                g.DrawString(Code, font, brush, 3, 2);
+
+                //画图片的前景干扰点
+                for (var i = 0; i < 100; i++)
+                {
+                    var x = random.Next(image.Width);
+                    var y = random.Next(image.Height);
+                    image.SetPixel(x, y, Color.FromArgb(random.Next()));
+                }
+
+                //画图片的边框线
+                g.DrawRectangle(new Pen(Color.Silver), 0, 0, image.Width - 1, image.Height - 1);
+
+                //保存图片数据
+                var stream = new MemoryStream();
+                image.Save(stream, ImageFormat.Jpeg);
+                //输出图片流
+                return stream.ToArray();
+            }
+            finally
+            {
+                g.Dispose();
+                image.Dispose();
+            }
+        }
+    }
+}
